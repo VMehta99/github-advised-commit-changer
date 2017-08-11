f241d62dc092a5a2a58c8f6b7b5d6498056c8962
Rishi Masand <masandrishi@gmail.com>
2017-08-11 11:17:15 -0400

    first commit

---
+++ change.js
@@ -0,0 +1,2 @@
+document.getElementsByClassName("user-select-contain")[2].innerHTML = "git add .";
+document.getElementsByClassName("user-select-contain")[0].parentElement.removeChild(document.getElementsByClassName("user-select-contain")[0]);
